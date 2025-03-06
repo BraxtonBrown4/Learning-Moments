30f4fd1a942d44e81ecfdaaeb8cf2220e54793c9
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-06 11:07:54 -0600

    finished functionality of edit profile

--- src/components/forms/ProfileEdit.jsx
+++ src/components/forms/ProfileEdit.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import "./Forms.css"
-import { getUserById } from "../../services/userService"
+import { getUserById, updateUserById } from "../../services/userService"
 import { useNavigate } from "react-router-dom"
 
 export const ProfileEdit = ({ currentUser }) => {
     const [profile, setProfile] = useState({})
+    const [fullName, setFullName] = useState('')
+    const [cohortNum, setCohortNum] = useState(0)
 
     const navigate = useNavigate()
 
@@ -14,23 +16,38 @@ export const ProfileEdit = ({ currentUser }) => {
         })
     }, [currentUser])
 
+    useEffect(()=>{
+        setFullName(profile?.fullName)
+        setCohortNum(profile?.cohort)
+    }, [profile])
+
+    const handleSaveBtn = () => {
+        updateUserById(
+            {
+                id: profile.id,
+                fullName: fullName,
+                cohort: cohortNum,
+            }
+        ).then(navigate(`/profile/${profile.id}`))
+    }
+
     return (
         <div className="profile-container">
             <div className="profile-box">
                 <div className="text-container">
                     <h2 className="text-field-description">Full Name</h2>
-                    <input type="text" className="text-field text-input" value={profile?.fullName}/>
+                    <input type="text" className="text-field text-input" onChange={(event)=>{setFullName(event.target.value)}} value={fullName}/>
                 </div>
                 <div className="text-container">
                     <h2 className="text-field-description">Cohort</h2>
-                    <input className="text-field text-input" value={profile?.cohort}/>
+                    <input type="number"className="text-field text-input" onChange={(event)=>{setCohortNum(event.target.value)}} value={cohortNum}/>
                 </div>
                 <div className="text-container">
                     <h2 className="text-field-description">Number Of Posts</h2>
                     <div className="text-field">{profile?.posts?.length}</div>
                 </div>
                 <div className="control-btns">
-                    <button type="button" className="btn btn-success control-btn">Save</button>
+                    <button type="button" className="btn btn-success control-btn" onClick={handleSaveBtn} >Save</button>
                     <button type="button" className="btn btn-danger control-btn" onClick={() => { navigate(`/profile/${profile.id}`) }}>Cancel</button>
                 </div>
             </div>
